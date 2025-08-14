f45f1c1ce7bd244943103f14344cff314dc723b8
Bottle414 <164286466+Bottle414@users.noreply.github.com>
2025-08-14 13:09:03 +0800

    fix(plugin): fix issue in rewriting history methods

--- packages/plugins/route.ts
+++ packages/plugins/route.ts
@@ -16,27 +16,31 @@ const RoutePlugin = ({
         // 重写 pushState
         const rowPush = history.pushState
         history.pushState = (...args) => {
-            rowPush.apply(this, args)
+            rowPush.apply(history, args)
             eventBus.emit('bottle-monitor:transport', CATEGORY.USER, {
                 type: USER.HISTORY_ROUTE,
+                method: 'pushState',
                 url: location.href
             })
         }
         // 重写 replaceState
         const rowReplace = history.replaceState
         history.replaceState = (...args) => {
-            rowReplace.apply(this, args)
+            rowReplace.apply(history, args)
             eventBus.emit('bottle-monitor:transport', CATEGORY.USER, {
                 type: USER.HASH_ROUTE,
+                method: 'replaceState',
                 url: location.href
             })
         }
     }
 
+    // SPA 路由跳转不触发此事件，前进后退才会
     const handleSPAHistory = () => {
         window.addEventListener('popstate', () => {
             eventBus.emit('bottle-monitor:transport', CATEGORY.USER, {
                 type: USER.HISTORY_ROUTE,
+                method: 'popstate',
                 url: location.href
             })
         })
@@ -47,6 +51,14 @@ const RoutePlugin = ({
         handleSPAHistory()
     }
 
+    /**
+     * ISSUE:
+     * 1. hash 改变了 history 也会监听到
+     * 2. 跳转到别的页面，整个界面刷新后不知道收到消息没
+     * 3. 不知道从哪里跳到哪里去了
+     * 
+     * 1. 对于 errorPlugin, 有些静默选项未生效
+     */
     const captureHashRoute = () => {
         window.addEventListener('hashchange', () => {
             eventBus.emit('bottle-monitor:transport', CATEGORY.USER, {
