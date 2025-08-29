9edb2777b7780d28318e28202fc3ddb0c9ebe173
一勺 <116160870+ZRMYDYCG@users.noreply.github.com>
2025-08-29 00:58:52 +0000

    feat(config): reasonably configure the tsconfig of sub-packages and integrate them into the root.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,10 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
+  globals: {},
   rules: {
     'no-console': 'off',
+    'unused-imports/no-unused-vars': ['warn', { varsIgnorePattern: '^_' }],
+    'node/prefer-global/process': 'off',
   },
 })
