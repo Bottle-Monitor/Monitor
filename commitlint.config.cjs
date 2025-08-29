983e5cb7a2f03fcc20e6ecc004951fe6a10dba56
一勺 <116160870+ZRMYDYCG@users.noreply.github.com>
2025-08-29 03:16:27 +0000

    perf(package): cohesively integrate the dependencies of the core package and migrate it from the monorepo.

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'subject-full-stop': [0], // 允许主题末尾使用句号
+    'header-max-length': [2, 'always', 306] // 允许标题的最大长度为 106，并启用错误级别检查
   },
 }
