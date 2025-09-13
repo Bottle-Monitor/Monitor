5f890ca666addd1f101bdb531bc7c8f8f1977521
Bottle414 <164286466+Bottle414@users.noreply.github.com>
2025-09-13 22:24:45 +0800

    feat(docs): add parallel register components function

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,17 +1,26 @@
 import DefaultTheme from 'vitepress/theme'
-import CodeDemo from './components/CodeDemo.vue'
-import FeatureCard from './components/FeatureCard.vue'
-import HomeHero from './components/HomeHero.vue'
-import TechShowcase from './components/TechShowcase.vue'
 import './styles/vars.css'
 import './styles/overrides.css'
 
+const components = [
+  'HomeHero',
+  'FeatureCard',
+  'TechShowcase',
+  'CodeDemo',
+  'FeatureCards',
+  'ShowCase',
+  'StatsCards',
+]
+
 export default {
   ...DefaultTheme,
-  enhanceApp({ app }) {
-    app.component('HomeHero', HomeHero)
-    app.component('FeatureCard', FeatureCard)
-    app.component('TechShowcase', TechShowcase)
-    app.component('CodeDemo', CodeDemo)
+  enhanceApp: async ({ app }) => {
+    const imports = components.map(name =>
+      import(`./components/${name}.vue`).then((module) => {
+        app.component(name, module.default)
+      }),
+    )
+
+    await Promise.all(imports)
   },
 }
